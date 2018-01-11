7399e8d1770fb79adde07b3338db6dff544be6ad
GrimArbre <34634575+GrimArbre@users.noreply.github.com>
2018-01-11 22:48:13 +0700

    title screen + menu

---
+++ TitleScreen.java
@@ -0,0 +1,28 @@
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
+
+
+
+public class TitleScreen {
+
+    private Image start, exit;
+    private boolean ok;
+
+    TitleScreen(){
+        start = new Image("file:resources/start.png");
+        exit= new Image("file:resources/exit.png");
+        ok = true;
+    }
+
+    public void draw(GraphicsContext context){
+        //context.clearRect(0, 0, 1280, 720);
+        if (ok)
+            context.drawImage(start, 0, 0);
+        else
+            context.drawImage(exit, 0, 0);
+        }
+
+    public void changeOK(){
+        ok = !ok;
+    }
+}
